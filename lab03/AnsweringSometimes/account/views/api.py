615c568bae8c8d4ea9f6b9723dcad95eea91efd7
Ilya Mazin <mazin.ia@bk.ru>
2022-06-21 22:08:18 +0300

    impl account/data

--- lab03/AnsweringSometimes/account/views/api.py
+++ lab03/AnsweringSometimes/account/views/api.py
@@ -1,10 +1,14 @@
-from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import permissions
+from account.models import User, UserSerializer
 
+@api_view(['GET', 'POST'])
+@permission_classes([permissions.IsAuthenticated])
 def account_data(request):
-    return JsonResponse({
-        'name': 'John',
-        'last_name': 'Snow'
-    })
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 # returns 'items_size' latest items from search history
 #   skipping first 'items_size * items_idx'
--- lab03/AnsweringSometimes/search_backend/models.py
+++ lab03/AnsweringSometimes/search_backend/models.py
@@ -1,26 +1,6 @@
 from django.db import models
-from account.models import User
+from account.models import User, UserSerializer
 from rest_framework import serializers
-
-# Create your models here.
-class History(models.Model):
-    user = models.ForeignKey(User, models.DO_NOTHING, blank=False)
-    item = models.ForeignKey('SearchItem', models.DO_NOTHING, blank=False)
-
-    results_clicks_num = models.IntegerField(blank=False)
-    marketing_clicks_num = models.IntegerField(blank=False)
-    pages_viewed_num = models.IntegerField(blank=False)
-
-    class Meta:
-        db_table = 'history'
-
-class HistorySerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.PrimaryKeyRelatedField()
-    item = serializers.PrimaryKeyRelatedField()
-
-    class Meta:
-        model = History
-        fields = ['user', 'item', 'results_clicks_num', 'marketing_clicks_num', 'pages_viewed_num']
 
 
 class SearchItem(models.Model):
@@ -34,3 +14,23 @@ class SearchItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = SearchItem
         fields = ['question', 'results_num']
+
+
+class History(models.Model):
+    user = models.ForeignKey(User, models.DO_NOTHING, blank=False)
+    item = models.ForeignKey(SearchItem, models.DO_NOTHING, blank=False)
+
+    results_clicks_num = models.IntegerField(blank=False)
+    marketing_clicks_num = models.IntegerField(blank=False)
+    pages_viewed_num = models.IntegerField(blank=False)
+
+    class Meta:
+        db_table = 'history'
+
+class HistorySerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+    item = SearchItemSerializer()
+
+    class Meta:
+        model = History
+        fields = ['user', 'item', 'results_clicks_num', 'marketing_clicks_num', 'pages_viewed_num']
