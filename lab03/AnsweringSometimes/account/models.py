aa9afe942b4cc04b0ca4cd9d233f195c5d7a9811
Ilya Mazin <mazin.ia@bk.ru>
2022-06-21 15:32:28 +0300

    give up miserably

--- lab03/AnsweringSometimes/account/models.py
+++ lab03/AnsweringSometimes/account/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
 
@@ -11,12 +11,7 @@ def validate_age(age):
         )
 
 # Create your models here.
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=20, blank=False, unique=True)
-    USERNAME_FIELD = 'username'
-
-    email = models.CharField(max_length=30, blank=False, unique=True)
-
+class User(AbstractUser):
     age = models.SmallIntegerField(
         validators=[validate_age],
         blank=False
@@ -32,11 +27,6 @@ class User(AbstractBaseUser):
         choices=Gender.choices,
         blank=False
     )
-
-    name = models.CharField(max_length=20, blank=False)
-    last_name = models.CharField(max_length=20, blank=False)
-    
-    REQUIRED_FIELDS = ['name', 'last_name', 'age', 'gender']
 
     class Meta:
         db_table = 'users'
